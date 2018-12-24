a46d24dcaff171da9f7f7b7834f7ea3efa4aab0d
IronHeart7334 <greengrappler12@gmail.com>
2018-12-24 08:59:42 -0800

    Updating to ES6
    export / import doesn't work because of CORS

--- scripts/block.js
+++ scripts/block.js
@@ -3,7 +3,7 @@ The Block class is used to generate the platforms that the player interacts with
 */
 
 /*
- * TODO: 
+ * TODO:
  * -change BLOCK_SIZE, don't want global. Maybe set in Game since it keeps track of the canvas size?
  * -maybe add special blocks i.e. IceBlock with special collide(entity) function that slips entities around?
  * -change to use Sprite instead of current block 'image'
@@ -11,54 +11,54 @@ The Block class is used to generate the platforms that the player interacts with
 
 var BLOCK_SIZE = canvas_size / 10; //may want to redo this as a strict size
 
-function Block(baseColor, rimColor, x, y) {
-    /*
-    baseColor: the color of this block
-    rimColor: the color of the frame around the block
-    x: the number of blocks between this one and the leftmost side of the area it is in
-    y: the number of blocks above this one and the top of the area it is in
-    hasBlockAbove: whether or not there is a block directly above this one, if it is true, 
-                    then this won't make top collision checks to prevent wall climbing
-    */
-    Entity.call(this);
-    this.setCoords(x * BLOCK_SIZE, y * BLOCK_SIZE);
-    this.baseColor = baseColor;
-    this.rimColor = rimColor;
-    this.hasBlockAbove = false;
-}
-Block.prototype = {
-    disableTop : function(){
+class Block extends Entity{
+    constructor(baseColor, rimColor, x, y) {
+      /*
+      baseColor: the color of this block
+      rimColor: the color of the frame around the block
+      x: the number of blocks between this one and the leftmost side of the area it is in
+      y: the number of blocks above this one and the top of the area it is in
+      hasBlockAbove: whether or not there is a block directly above this one, if it is true,
+                      then this won't make top collision checks to prevent wall climbing
+      */
+      super();
+      this.setCoords(x * BLOCK_SIZE, y * BLOCK_SIZE);
+      this.baseColor = baseColor;
+      this.rimColor = rimColor;
+      this.hasBlockAbove = false;
+    }
+    disableTop(){
         //prevents top collisions when there is a block above this one
         //invoked by Area
         this.hasBlockAbove = true;
-    },
-    draw : function(canvas){
+    }
+    draw(canvas){
         var shift = BLOCK_SIZE / 10;
         canvas.setColor(this.rimColor);
         canvas.rect(this.x, this.y, BLOCK_SIZE, BLOCK_SIZE);
         canvas.setColor(this.baseColor);
-        canvas.rect(this.x + shift, this.y + shift, BLOCK_SIZE - shift * 2, BLOCK_SIZE - shift * 2);	
-    },
-    shoveOut : function(entity){
+        canvas.rect(this.x + shift, this.y + shift, BLOCK_SIZE - shift * 2, BLOCK_SIZE - shift * 2);
+    }
+    shoveOut(entity){
         // yay! finally works!
-        // 
+        //
         // the end coordinates of a line between this and entity, but shifted to (0, 0)
         var xDiff = (entity.x + entity.width / 2) - (this.x + this.width / 2);
         var yDiff = (entity.y + entity.height / 2) - (this.y + this.height / 2);
-        
+
         // the length of the sides of the triangle
         var x = Math.abs(xDiff);
         var y = Math.abs(yDiff);
-        
+
         var alpha;
-        
+
         if(x === 0){
             //prevent undefined
-            alpha = 90; 
+            alpha = 90;
         } else {
             alpha = Math.atan(y / x) * 180 / Math.PI;
         }
-        
+
         var theta;
         //since I'm not performing trig after this, these are their angles on the canvas, NOT CARTESIAN
         if(xDiff > 0 && yDiff < 0){
@@ -70,14 +70,14 @@ Block.prototype = {
         } else {
             theta = 360 - alpha;
         }
-        
+
         if(xDiff === 0){
             theta = (yDiff > 0) ? 270 : 90;
         }
         if(yDiff === 0){
             theta = (xDiff > 0) ? 0 : 180;
         }
-        
+
         if(!this.hasBlockAbove && between(45, theta, 135)){
             //top
             entity.setY(this.y - entity.height);
@@ -94,7 +94,7 @@ Block.prototype = {
         } else {
             console.log("? " + theta);
         }
-    },
+    }
     checkColl(entity){
         var ret = false;
         if(this.checkForCollide(entity)){
@@ -102,22 +102,21 @@ Block.prototype = {
            ret = true;
         }
         return ret;
-    }	
+    }
 };
-extend(Block, Entity);
-
 
 // subclasses
-function MetalBlock(x, y){
-    Block.call(this, silver(4), silver(3), x, y);
+class MetalBlock extends Block{
+    constructor(x, y){
+      super(silver(4), silver(3), x, y);
+    }
 }
-extend(MetalBlock, Block);
 
-function GoldBlock(x, y){
-    Block.call(this, gold(10), gold(4), x, y);
+class GoldBlock extends Block{
+    constructor(x, y){
+      super(gold(10), gold(4), x, y);
+    }
 }
-extend(GoldBlock, Block);
-
 
 // test functions
 function testAllColorFunction(f){
@@ -141,12 +140,12 @@ function testAllGold(){
 function testShove(){
     var b = new GoldBlock(0, 0);
     var e = new Entity();
-    
+
     var top = [];
     var bottom = [];
     var left = [];
     var right = [];
-    
+
     console.log(b.x + ", " + b.y);
     for(var i = 0; i <= 360; i+=15){
         console.log(i + ":");
